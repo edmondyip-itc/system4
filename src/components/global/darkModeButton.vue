dbc6718cd5c080b75687ff84689f645f8ae354ce
Edmond Yip <edmond.yip@itccompliance.co.uk>
2024-06-06 15:14:11 +0100

    update button

--- src/components/global/darkModeButton.vue
+++ src/components/global/darkModeButton.vue
@@ -1,16 +1,15 @@
 <script setup>
 import { useDark, useToggle } from '@vueuse/core'
 import Button from '../ui/button/Button.vue';
-import IconSun from '@/components/icons/IconSun.vue'
-import IconMoon from '@/components/icons/IconMoon.vue'
+import { Icon } from '@iconify/vue'
 
 const isDark = useDark()
 const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <Button class="self-end" @click="toggleDark()" >
-    <IconSun v-if="!isDark" icon="ri:sun-fill" />
-    <IconMoon v-else icon="ri:moon-fill" />
+  <Button size="xs" class="self-end" @click="toggleDark()" >
+    <Icon v-if="!isDark" icon="ri:sun-fill" />
+    <Icon v-else icon="ri:moon-fill" />
   </Button>
 </template>
---
+++ src/components/global/logoutButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import Button from '../ui/button/Button.vue';
+import { useLogin } from '@/stores/login'
+
+const store = useLogin()
+const router = useRouter()
+
+const logout = () => {
+  store.loginAction(false)
+  router.push({ name: 'login'})
+}
+</script>
+
+<template>
+  <Button @click="logout" size="xs">Logout</Button>
+</template>
