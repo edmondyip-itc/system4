07203755f8c5c203321218c9a5cd628c5c8cc537
Edmond Yip <edmond.yip@itccompliance.co.uk>
2024-06-06 15:15:52 +0100

    fix bug

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'login',
       component: LoginView
     },
@@ -15,9 +16,9 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/home',
+      path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue')
+      component: HomeView
     }
   ]
 })
