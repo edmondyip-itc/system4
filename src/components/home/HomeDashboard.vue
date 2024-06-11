c6e56ee6adec3af3b397c5dd730297d2050d1f6d
Edmond Yip <edmond.yip@itccompliance.co.uk>
2024-06-11 17:41:16 +0100

    add dashboard

---
+++ src/components/home/HomeDashboard.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+</script>
+
+<template>
+  <Tabs default-value="all">
+    <TabsList class="bg-inherit">
+      <TabsTrigger value="all" class="text-3xl">
+        All
+      </TabsTrigger>
+      <TabsTrigger value="apex-v3">
+        ApexV3
+      </TabsTrigger>
+      <TabsTrigger value="system4">
+        System4
+      </TabsTrigger>
+      <TabsTrigger value="product">
+        Product
+      </TabsTrigger>
+    </TabsList>
+    <TabsContent value="all">
+      All
+    </TabsContent>
+    <TabsContent value="apex-v3">
+      Apex V3
+    </TabsContent>
+    <TabsContent value="system4">
+      System4
+    </TabsContent>
+    <TabsContent value="product">
+      Product
+    </TabsContent>
+  </Tabs>
+</template>
