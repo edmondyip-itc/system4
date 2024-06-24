0d352387d9aa2e4ba30a71d12d6543ec18dae62f
Edmond Yip <edmond.yip@itccompliance.co.uk>
2024-06-24 16:37:04 +0100

    able to close welcome msg

--- src/components/home/HomeWelcome.vue
+++ src/components/home/HomeWelcome.vue
@@ -1,10 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+import { Icon } from '@iconify/vue'
+import { Card, CardHeader, CardTitle, CardDescription } from '@/components/ui/card'
+
+const show = ref(true)
+const close = () => {
+  return show.value = false
+}
+</script>
+
 <template>
-  <section class="py-8">
-    <header>
-      <h1>
-        Hello Paul! <br/>  Welcome Back to System4! 👋
-      </h1>
-      <span class="text-gray-600">Have a nice day at work today</span>
-    </header>
+  <section v-if="show">
+    <Card class="bg-background/50 relative">
+      <CardHeader>
+        <CardTitle class="text-3xl">
+          Hello Paul! <br/>  Welcome Back to System4! 👋
+        </CardTitle>
+        <CardDescription>
+          <span>Have a nice day at work today</span>
+        </CardDescription>
+        <Icon
+          @click="close"
+          class="text-2xl absolute top-2 right-2 cursor-pointer"
+          icon="ri:close-fill"
+        />
+      </CardHeader>
+    </Card>
   </section>
 </template>
