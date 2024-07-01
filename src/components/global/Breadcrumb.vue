5bed91294ab923d6376604c6124b28bc11184845
Edmond Yip <edmond.yip@itccompliance.co.uk>
2024-07-01 10:41:19 +0100

    update breadcurmb

--- src/components/global/Breadcrumb.vue
+++ src/components/global/Breadcrumb.vue
@@ -1,33 +1,43 @@
 <script setup>
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '@/components/ui/breadcrumb'
+import { Icon } from '@iconify/vue'
+import { productList } from '@/lib/apps';
 
-const props = defineProps(['info'])
+const props = defineProps(['appId', 'pageInfo'])
+const productInfo = productList[props.appId]
+const hasSubsection = productInfo.subSections.length > 0
+const subSectionInfo = (id) => {
+  return props.pageInfo?.tags.includes(id)
+}
 </script>
 
 <template>
-  <Breadcrumb class="bg-background p-2 rounded-lg mb-4">
+  <Breadcrumb class=" p-2 rounded-lg mb-4">
     <BreadcrumbList>
       <BreadcrumbItem>
         <BreadcrumbLink href="/">
-          Home
+          <Icon class="text-xl" icon="ri:home-smile-2-fill" />
         </BreadcrumbLink>
       </BreadcrumbItem>
       <BreadcrumbSeparator />
-      <BreadcrumbItem>
-        <BreadcrumbLink href="/components">
-          Components
+      <BreadcrumbItem v-if="productInfo">
+        <BreadcrumbLink :href="productInfo.path">
+          {{ productInfo.label }}
         </BreadcrumbLink>
       </BreadcrumbItem>
+      <template v-if="hasSubsection" v-for="subSection in productInfo.subSections">
+        <template v-if="subSectionInfo(subSection.id)">
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink :href="productInfo.path">
+              {{ subSection.label }}
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </template>
+      </template>
       <BreadcrumbSeparator />
       <BreadcrumbItem>
-        <BreadcrumbPage>{{ info.label }}</BreadcrumbPage>
+        <BreadcrumbPage v-if="pageInfo">{{ pageInfo.label }}</BreadcrumbPage>
       </BreadcrumbItem>
     </BreadcrumbList>
   </Breadcrumb>
